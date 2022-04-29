25d2c813475d79a275d0a6ba64ec579606160299
williamDmartins <102559101+williamDmartins@users.noreply.github.com>
2022-04-28 21:26:52 -0300

    Add files via upload

---
+++ Inferencia.java
@@ -0,0 +1,21 @@
+package fundamentos;
+
+public class Inferencia {
+	
+	public static void main(String[] args) {
+
+		var a = 13;
+		System.out.println(a);
+		
+		var b = 13.05;
+		System.out.println(b);
+		
+		var c = "texte";
+		System.out.println(c);
+		
+		var d = "aula";
+		d = "outra aula";
+		System.out.println(d);
+	}
+
+}
